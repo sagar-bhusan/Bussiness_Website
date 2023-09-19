7c2a773a657f4283fc4ee107ce89bb7b88f50cf2
Sagar Bhusan <sagarkgp1@gmail.com>
2023-09-19 23:48:35 +0530

    Added gallery items changes in html, js, css files

--- myscript.js
+++ myscript.js
@@ -44,24 +44,44 @@ window.addEventListener('load', function () {
 
 
 // JavaScript code for gallery slider movement
-    $(document).ready(function() {
-        $('.gallery-slider').slick({
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 3000,
-            centerMode: true,
-            centerPadding: '0',
-            responsive: [
-                {
-                    breakpoint: 768,
-                    settings: {
-                        slidesToShow: 1
-                    }
-                }
-            ]
-        });
-    });
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let active = 0;
+next.onclick = function () {
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function () {
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+let refreshInterval = setInterval(() => { next.click() }, 3000);
+function reloadSlider() {
+    slider.style.left = -items[active].offsetLeft + 'px';
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(() => { next.click() }, 3000);
+
+
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', () => {
+        active = key;
+        reloadSlider();
+    })
+})
+window.onresize = function (event) {
+    reloadSlider();
+};
 
 
 
